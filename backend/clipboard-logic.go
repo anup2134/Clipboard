6c0751c2c3c83dc3c409b18a24e08aba3f8ee24a
anup <anup.s.bhoos@gmail.com>
2025-06-10 00:36:37 +0530

    Add paste feature, preserve clipboard history as a json in a text file

--- backend/clipboard-logic.go
+++ backend/clipboard-logic.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"encoding/json"
+	"os"
 	"os/exec"
+	"os/signal"
 	"strings"
+	"syscall"
 	"time"
 )
 
@@ -34,15 +38,61 @@ func getClipboardHistory() []string{
 	return clipboardHistory
 }
 
-func main(){
-	go connection()
-	for{
-		content, err := getClipboard()
-		
-		if err == nil  && strings.TrimSpace(content) != "" {
-			addText(content)
-		}
-
-		time.Sleep(500 * time.Millisecond)
+func saveClipboardHistoy(){
+	f,err := os.OpenFile("/home/anup/.clipboardHistory", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil{
+		panic(err)
 	}
+	defer f.Close()
+
+	jsonBytes,err := json.Marshal(clipboardHistory)
+	if err != nil{
+		panic(err)
+	}
+	
+	_, err = f.WriteString(string(jsonBytes))
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main(){
+	data,err := os.ReadFile("/home/anup/.clipboardHistory")
+	if err!=nil{
+		panic(err)
+	}
+	
+	var arr []string
+	err = json.Unmarshal(data,&arr)
+	if err != nil{
+		panic(err)
+	}
+
+	clipboardHistory = append(clipboardHistory, arr...)
+
+	go connection()
+	defer saveClipboardHistoy()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+
+	stop := make(chan bool)
+	
+	go func(){
+		for{
+			select{
+				case <-stop:
+					return
+				default:
+					content, err := getClipboard()
+					if err == nil  && strings.TrimSpace(content) != "" {
+						addText(content)
+					}
+					time.Sleep(500 * time.Millisecond)
+			}
+		}
+	}()
+
+	<-c
+	close(stop)
 }
